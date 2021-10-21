26bdc326dd31be4c76dd900f5a6bccf42c6faa7b
ParaMattYT <91027977+ParaMattYT@users.noreply.github.com>
2021-10-21 11:12:18 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,123 @@
+namespace SpriteKind {
+    export const TREE = SpriteKind.create()
+    export const king = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Food, SpriteKind.king, function (sprite, otherSprite) {
+    eg.setPosition(randint(0, 153), randint(0, 113))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    eg.setPosition(randint(0, 153), randint(0, 113))
+    info.changeScoreBy(1)
+    music.baDing.play()
+})
+sprites.onOverlap(SpriteKind.Food, SpriteKind.TREE, function (sprite, otherSprite) {
+    eg.setPosition(randint(0, 153), randint(0, 113))
+})
+let eg: Sprite = null
+info.setScore(0)
+scene.setBackgroundImage(assets.image`PigBackground`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+animation.runImageAnimation(
+mySprite,
+assets.animation`pigIdle`,
+200,
+true
+)
+mySprite.setPosition(18, 81)
+let TREE = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.TREE)
+animation.runImageAnimation(
+TREE,
+assets.animation`T R E E`,
+200,
+true
+)
+mySprite.setStayInScreen(true)
+TREE.setPosition(117, 48)
+eg = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+animation.runImageAnimation(
+eg,
+assets.animation`eg`,
+200,
+true
+)
+eg.setStayInScreen(true)
+let KING = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.king)
+animation.runImageAnimation(
+KING,
+assets.animation`kingPigIdle`,
+200,
+true
+)
+KING.setPosition(10, 102)
+forever(function () {
+    music.playMelody("E E F D E E F D ", 120)
+})
